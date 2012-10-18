cb7bf897797883162c3ad25ac1517cd74750df1b
Italo Maia <italo.maia@gmail.com>
2012-10-18 00:09:00 -0300

    added setup.py. pip friendly.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-renamer',
+    version='0.1',
+    description='Pyrenamer for command-line.',
+    author='Italo Maia',
+    url='http://italomaia.com',
+    scripts=['renamer.py']
+)
